8497f8fef761dda158e3986013e61b51c508815c
Luis Sanchez Barranquero <luis.sb.dev@gmail.com>
2021-12-22 22:06:28 +0000

    Simplifying project structure

--- scripts/utilities/math.js
+++ scripts/utilities/math.js
@@ -4,90 +4,71 @@ class Vector{
       this.x = x;
       this.y = y;
     }
-  
+
     //Pseudo-properties
     get length(){
       return Math.sqrt(this.x * this.x + this.y * this.y);
     }
-  
+
     //Instance Methods
     add(other){
       try{
         this.x += other.x;
         this.y += other.y;
       }
-      catch(error){
-        console.log(`ERROR: ${error}`);
+      catch(err){
+        console.log(err);
       }
     }
-  
+
     sub(other){
       try{
         this.x -= other.x;
         this.y -= other.y;
       }
-      catch(error){
-        console.log(`ERROR: ${error}`);
+      catch(err){
+        console.log(err);
       }
     }
-  
+
     mult(scalar){
       this.x *= scalar;
       this.y *= scalar;
     }
-  
+
     div(scalar){
       this.x /= scalar;
       this.y /= scalar;
     }
-  
+
     normalize(){
       this.x /= this.length;
       this.y /= this.length;
     }
+
     //Static Methods
     static add(v1, v2){
-      let result = false;
-  
-      try{
-        result = new Vector(v1.x + v2.x, v1.y + v2.y);
-      }
-      catch(error){
-        console.log(`ERROR: ${error}`);
-      }
-      finally{
-        return result;
-      }
+	  return new Vector(v1.x + v2.x, v1.y + v2.y);
     }
-  
+
     static sub(v1, v2){
-      let result = false;
-  
-      try{
-        result = new Vector(v1.x - v2.x, v1.y - v2.y);
-      }
-      catch(error){
-        console.log(`ERROR: ${error}`);
-      }
-      finally{
-        return result;
-      }
+      return result = new Vector(v1.x - v2.x, v1.y - v2.y);
     }
-  
+
     static mult(v1, scalar){
       return new Vector(v1.x * scalar, v1.y * scalar);
     }
-  
+
     static div(v1, scalar){
       return new Vector(v1.x / scalar, v1.y / scalar);
     }
-  
+
     static dot(v1, v2){
       return v1.x * v2.x + v1.y * v2.y;
     }
-  
+
     static normalize(v1){
       return new Vector(v1.x / v1.length, v1.y / v1.length);
     }
   }
-  
+
